f291da5c761b2f30ba09b418666c8bd973644a62
Victor Hugo <victorhugo.mota26@gmail.com>
2025-07-24 15:45:25 -0300

    refactor

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -32,10 +32,7 @@ public class PrincipalComListas {
             }
         }
 
-
-
-
-
+        //""
 
         ArrayList<String> buscaPorArtista = new ArrayList<>();
         buscaPorArtista.add("Adam Sandler");
